3c14ac236b108f813b0b7ddc7d6f1833e9578be1
Petter Eriksson <ipetterik@gmail.com>
2017-08-29 17:15:11 -0400

    Next iteration SNAPSHOT-5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.petterik/om "1.0.0-alpha49-SNAPSHOT-4"
+(defproject org.clojars.petterik/om "1.0.0-alpha49-SNAPSHOT-5"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
